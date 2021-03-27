236f5724b77b91c16d560e32e7adf00df42b636b
irene <irenepopovkina@gmail.com>
2021-03-27 13:23:55 -0700

    changed smokeFunctional

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,5 +1,6 @@
 import sel from '../../data/selectors';
-import exp from '../../data/expected.json';
+import {name, gender, age, story} from '../../data/testData';
+import inputValues4 from '../../helpers/methods';
 
 describe('Required fields and story created', function () {
     before('Open App', function () {
@@ -7,11 +8,11 @@ describe('Required fields and story created', function () {
     });
 
     it('TC-026 Submit button is enabled after fields 1-4 are filled in with valid values', function () {
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
+        $(sel.name).setValue(name.default);
+        $$(sel.radioButtons)[gender.she].click();
+        $(sel.age).setValue(age.default);
         $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        $$(sel.storyList)[story.comedy].click();
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(true);
     });
@@ -19,11 +20,8 @@ describe('Required fields and story created', function () {
     it('TC-027 User can create a story with valid values', function () {
         browser.refresh();
 
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
-        $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        inputValues4(name.default, gender.she, age.default, story.comedy)
+
         $(sel.submit).click();
         let tryAgainBtn = $(sel.tryAgain).isDisplayed();
         expect(tryAgainBtn).toEqual(true);
